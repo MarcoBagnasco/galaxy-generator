dc6a0d17c7bd0191010b8ea86b6fb7ee15790492
MarcoBagnasco <bagna.hbk@gmail.com>
2021-08-12 21:17:16 +0200

    custom particles geometry

--- src/script.js
+++ src/script.js
@@ -24,11 +24,20 @@ const textureLoader = new THREE.TextureLoader();
  * Particles
  */
 // Geometrty
-const particlesGeometry = new THREE.SphereBufferGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 5000;
 
+const positions = new Float32Array(count * 3);
+
+for(let i = 0; i < count * 3; i++) 
+{
+    positions[i] = (Math.random() - .5) * 10;
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-    size: .02,
+    size: .1,
     sizeAttenuation: true,
 }); 
 
